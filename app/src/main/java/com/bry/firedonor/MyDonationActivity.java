3a686d73c7290a1c751b52a64e2b91201e8b55c1
bryonyoni <bryonyoni@gmail.com>
2019-07-07 00:12:34 +0300

    Finished up on the my donations, requests and edit donation views.

---
+++ app/src/main/java/com/bry/firedonor/MyDonationActivity.java
@@ -0,0 +1,13 @@
+package com.bry.firedonor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyDonationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_donation2);
+    }
+}
